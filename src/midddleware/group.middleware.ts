fca7bc5a0b045904a53424fce7423b8360035226
Kritarth Sharma <kritarth2121@gmail.com>
2021-08-11 10:55:26 +0530

    done with 2 approach of loading existing data and then querying fresh data in backend

--- src/midddleware/group.middleware.ts
+++ src/midddleware/group.middleware.ts
@@ -7,21 +7,22 @@ import { store } from "../store";
 export const fetchgroups = (request: GroupRequest) => {
     const query = request.query;
     //const state=store.getState();
-    const queryMap = groupQueryMapSelector(store.getState());
+    //const queryMap = groupQueryMapSelector(store.getState());
 
-    const groupIDS = queryMap[query!];
-    groupActions.groupAction(query!, !groupIDS);
+    // const groupIDS = queryMap[query!];
+    // groupActions.groupAction(query!, !groupIDS);
     // const queryloading=groupQueryLoading(state);
-    // const loading = query.loading;
+    const loading = groupLoading[query]
     // const tokenSource = axios.CancelToken.source();
     // cancelationTokenSources[query] = tokenSource;
-    if (groupIDS) {
+    groupActions.query(query);
+    if (loading) {
         return;
     }
     fetchGroups(request).then((groups) => {
         const a = (groups as unknown) as Group[];
         console.log(a);
-        groupActions.groupsfetch(a, query!);
+        groupActions.groupsfetch(a.data, query!);
         //cancelationTokenSources[query] = undefined;
     });
 };
